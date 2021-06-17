0f18eb1724d9c6413ec0eba6ec079bff9a38d1da
WWU-BogdanConstantin <cbcons135@gmail.com>
2021-06-17 13:55:37 +0100

    Refactor AdvertisementService

--- BadProject/AdvertisementService.cs
+++ BadProject/AdvertisementService.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Runtime.Caching;
 using System.Threading;
+using BadProject.Providers;
+using BadProject.Services;
+using BadProject.Settings;
 using ThirdParty;
 
 namespace Adv
 {
     public class AdvertisementService
     {
-        private static MemoryCache cache = new MemoryCache("");
-        private static Queue<DateTime> errors = new Queue<DateTime>();
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly INoSqlAdvProvider _noSqlAdvProvider;
+        private readonly ISqlAdvProvider _sqlAdvProvider;
+        private readonly AppSettings _appSettings;
+
+        private readonly int RetryDelay;
+        private readonly int RetryCount;
+        private DateTimeOffset cacheOffset;
+        private readonly int ErrorThreshold;
+
+        private static MemoryCache cache;
+        private static Queue<DateTime> errors;
 
         private Object lockObj = new Object();
+
+        public AdvertisementService(IDateTimeProvider dateTimeProvider, INoSqlAdvProvider noSqlAdvProvider,
+            ISqlAdvProvider sqlAdvProvider, AppSettings appSettings, MemoryCache memoryCache, ErrorService errorService)
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _noSqlAdvProvider = noSqlAdvProvider;
+            _sqlAdvProvider = sqlAdvProvider;
+            RetryCount = appSettings.RetryCount;
+            RetryDelay = appSettings.RetryDelay;
+            ErrorThreshold = appSettings.ErrorThreshold;
+            cache = memoryCache;
+            errors = errorService.HttpErrors;
+            cacheOffset = dateTimeProvider.DateTimeOffsetNow.AddMinutes(5);
+        }
+
+        public AdvertisementService() : this(new DateTimeProvider(), new NoSqlAdvProviderProxy(new NoSqlAdvProvider()),
+            new SqlAdvProviderProxy(),
+            new AppSettings
+            {
+                RetryCount = int.Parse(ConfigurationManager.AppSettings["RetryDelay"]),
+                RetryDelay = int.Parse(ConfigurationManager.AppSettings["RetryDelay"])
+            }, new MemoryCache("default"),
+            new ErrorService {HttpErrors = new Queue<DateTime>()})
+        {
+        }
+
         // **************************************************************************************************
         // Loads Advertisement information by id
         // from cache or if not possible uses the "mainProvider" or if not possible uses the "backupProvider"
@@ -29,64 +68,67 @@ namespace Adv
         //    it uses the SqlDataProvider (backupProvider)
         public Advertisement GetAdvertisement(string id)
         {
-            Advertisement adv = null;
-
             lock (lockObj)
             {
-                // Use Cache if available
-                adv = (Advertisement)cache.Get($"AdvKey_{id}");
-
-                // Count HTTP error timestamps in the last hour
-                while (errors.Count > 20) errors.Dequeue();
-                int errorCount = 0;
-                foreach (var dat in errors)
-                {
-                    if (dat > DateTime.Now.AddHours(-1))
-                    {
-                        errorCount++;
-                    }
-                }
+                // Try Cache if available
+                var adv = GetCacheAdvertisement(id);
+                if (adv != null) return adv;
 
 
-                // If Cache is empty and ErrorCount<10 then use HTTP provider
-                if ((adv == null) && (errorCount < 10))
-                {
-                    int retry = 0;
-                    do
-                    {
-                        retry++;
-                        try
-                        {
-                            var dataProvider = new NoSqlAdvProvider();
-                            adv = dataProvider.GetAdv(id);
-                        }
-                        catch
-                        {
-                            Thread.Sleep(1000);
-                            errors.Enqueue(DateTime.Now); // Store HTTP error timestamp              
-                        }
-                    } while ((adv == null) && (retry < int.Parse(ConfigurationManager.AppSettings["RetryCount"])));
+                // Try HTTP Provider
+                adv = GetHttpAdvertisement(id);
+                if (adv != null) return adv;
 
 
-                    if (adv != null)
-                    {
-                        cache.Set($"AdvKey_{id}", adv, DateTimeOffset.Now.AddMinutes(5));
-                    }
-                }
+                // finally Try Backup provider
+                adv = GetSqlAdvertisement(id);
+                if (adv != null) AddAdvertisementToCache(adv);
 
-
-                // if needed try to use Backup provider
-                if (adv == null)
-                {
-                    adv = SQLAdvProvider.GetAdv(id);
-
-                    if (adv != null)
-                    {
-                        cache.Set($"AdvKey_{id}", adv, DateTimeOffset.Now.AddMinutes(5));
-                    }
-                }
+                return adv;
             }
-            return adv;
+        }
+        private Advertisement GetCacheAdvertisement(string id)
+        {
+            return (Advertisement) cache.Get($"AdvKey_{id}");
+        }
+        private bool TooManyErrors(int threshold)
+        {
+            while (errors.Count > threshold) errors.Dequeue();
+            return errors.Count > 0 && errors.Dequeue() > _dateTimeProvider.DateTimeNow.AddHours(-1);
+        }
+        private Advertisement GetHttpAdvertisement(string id)
+        {
+            Advertisement adv;
+            if (!TooManyErrors(ErrorThreshold))
+            {
+                int retry = 0;
+                do
+                {
+                    retry++;
+                    try
+                    {
+                        adv = _noSqlAdvProvider.GetAdv(id);
+                        if (adv == null) continue;
+                        AddAdvertisementToCache(adv);
+                        return adv;
+                    }
+                    catch
+                    {
+                        Thread.Sleep(RetryDelay);
+                        errors.Enqueue(_dateTimeProvider.DateTimeNow); // Store HTTP error timestamp              
+                    }
+                } while (retry < RetryCount);
+            }
+
+            return null;
+        }
+        private Advertisement GetSqlAdvertisement(string id)
+        {
+            return _sqlAdvProvider.GetAdv(id);
+        }
+        private void AddAdvertisementToCache(Advertisement advertisement)
+        {
+            cache.Set($"AdvKey_{advertisement.WebId}", advertisement, cacheOffset);
         }
     }
 }
