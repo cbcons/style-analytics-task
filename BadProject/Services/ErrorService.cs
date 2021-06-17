045c03ec7ec7e20530067ef1df5f3e89cf0e033a
WWU-BogdanConstantin <cbcons135@gmail.com>
2021-06-17 13:53:05 +0100

    Add ErrorService

---
+++ BadProject/Services/ErrorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BadProject.Services
+{
+    public class ErrorService
+    {
+        public Queue<DateTime> HttpErrors { get; set; }
+    }
+}
